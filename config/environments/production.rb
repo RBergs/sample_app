eea6bc9430a9fc242c395c71783e1a2b566e3502
Richard Berger <richard.h.berger@gmail.com>
2011-10-24 18:43:18 -0400

    Updating CSS for precompiling of assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,8 +42,8 @@ SampleApp::Application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
 
-  # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
+  Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
+  config.assets.precompile += %w( 'search.js', 'blueprint/screen.css', 'blueprint/print.css', 'custom.css' )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
